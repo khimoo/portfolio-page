b010c950e9d89dbe4e4c4a247dccc8254325cf71
khimoo <dailysentence1111@gmail.com>
2026-01-06 03:04:18 +0900

    basepath設定

--- khimoo-portfolio/src/web/app.rs
+++ khimoo-portfolio/src/web/app.rs
@@ -4,11 +4,19 @@ use crate::web::routes::Route;
 use crate::web::pages::{HomePage, ArticleIndexPage, ArticleViewPage};
 use crate::web::header::Header;
 use crate::web::styles::LayoutStyles;
+use crate::config::get_config;
 
 #[function_component(App)]
 pub fn app() -> Html {
+    let config = get_config();
+    let basename = if config.base_path.is_empty() {
+        None
+    } else {
+        Some(config.base_path.clone())
+    };
+
     html! {
-        <BrowserRouter>
+        <BrowserRouter basename={basename}>
             <div style={LayoutStyles::app_wrapper()}>
                 <Header />
                 <main style={LayoutStyles::main_content()}>
